b49e41e1f044162569c1dda3e86b6f3a122fc921
Lebohang mofokeng <105319206+LebohangLuuh@users.noreply.github.com>
2025-05-20 21:05:04 +0200

    navigation between login n register

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterModule } from '@angular/router';
+import { AppComponent } from "../../app.component";
+
+@Component({
+  selector: 'app-login',
+  imports: [RouterLink, RouterModule],
+  templateUrl: './login.component.html',
+  styles: ``
+})
+export class LoginComponent {
+
+}
---
+++ src/app/pages/register/register.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterModule } from '@angular/router';
+
+
+@Component({
+  selector: 'app-register',
+  imports: [RouterModule, RouterLink],
+  templateUrl: './register.component.html',
+})
+export class RegisterComponent {
+}
