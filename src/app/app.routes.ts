0c797ee4226f71819897f460d14c07395d285ab8
Lebohang mofokeng <105319206+LebohangLuuh@users.noreply.github.com>
2025-05-21 21:41:48 +0200

    added more component for social media clone

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,11 @@
 import { Routes, RouterModule } from '@angular/router';
 import { RegisterComponent } from './pages/register/register.component';
 import { LoginComponent } from './pages/login/login.component';
-import { HomeComponent } from './components/home/home.component';
-import { NgModule } from '@angular/core';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: '', redirectTo: 'login', pathMatch: 'full', },
-  { path: 'home', component: HomeComponent}
 ];
 
 
-//  @NgModule({
-//       imports: [RouterModule.forRoot(routes)],
-//       exports: [RouterModule],
-//     })
-//     export class AppRoutingModule {}
---
+++ src/app/posts/feed/feed.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-feed',
+  imports: [],
+  templateUrl: './feed.component.html',
+  styles: ``
+})
+export class FeedComponent {
+
+}
